f1ad16879acd40b08f2c1818bab1348c22c56f29
Rigo <rigoyao@outlook.com>
2022-12-11 21:57:35 -0500

    update train and model files

--- Transformer/model.py
+++ Transformer/model.py
@@ -3,137 +3,89 @@ import numpy as np
 import random
 
 class Generator(tf.keras.utils.Sequence) :
-    def __init__(self, all_song_tokenised, batch_size, sequence_length, val_split = 0, shuffle=True) :
-        self.all_song_tokenised = all_song_tokenised
-        self.pad_tag_idx = 1
-        self.sequence_length = sequence_length
+    def __init__(self, processed_dataset, batch_size, seq_len, val_split = 0, shuffle=True) :
+        self.processed_dataset = processed_dataset
+        self.seq_len = seq_len
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.val_split = val_split
         if(self.val_split != 0):
-            self.all_song_tokenised = random.choices(self.all_song_tokenised, k = int(self.val_split*len(self.all_song_tokenised)))
-            self.batch_size = len(self.all_song_tokenised)
+            self.processed_dataset = random.choices(self.processed_dataset, k = int(self.val_split*len(self.processed_dataset)))
+            self.batch_size = len(self.processed_dataset)
         self.on_epoch_end()
     
     def __len__(self) :
-        return int(np.ceil(len(self.all_song_tokenised)/ self.batch_size))
+        return int(np.ceil(len(self.processed_dataset)/ self.batch_size))
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
         if self.shuffle == True:
-            np.random.shuffle(self.all_song_tokenised)
+            np.random.shuffle(self.processed_dataset)
   
     def __getitem__(self, idx) :
-        batch_x = np.empty((0, self.sequence_length), float)
-        batch_y = np.empty((0, self.sequence_length), float)
+        batch_x = np.empty((0, self.seq_len), float)
+        batch_y = np.empty((0, self.seq_len), float)
         for i in range(self.batch_size):
-            if(idx*self.batch_size + i == len(self.all_song_tokenised)-1):
+            if(idx*self.batch_size + i == len(self.processed_dataset)-1):
                 return batch_x, batch_y
-            song = self.all_song_tokenised[idx*self.batch_size + i]
-            start_idx = random.randint(0,len(song) - self.sequence_length/2)
-            seq = song[start_idx: start_idx + self.sequence_length + 1]
-            x= seq[:-1]
+            song = self.processed_dataset[idx*self.batch_size + i]
+            start_idx = random.randint(0,len(song) - self.seq_len/2)
+            seq = song[start_idx: start_idx + self.seq_len + 1]
+            x = seq[:-1]
             y = seq[1:]
-            if(len(y) < self.sequence_length):
-                no_of_pad = self.sequence_length - len(y)
-                x = np.append(x, [self.pad_tag_idx]*no_of_pad, axis = 0)
-                y = np.append(y, [self.pad_tag_idx]*no_of_pad, axis = 0)
+            if(len(y) < self.seq_len):
+                sub = len(y)
+                num_pad = self.seq_len - sub
+                # 1 is the padding value
+                x = np.append(x, [1]*num_pad, axis = 0)
+                y = np.append(y, [1]*num_pad, axis = 0)
             
             batch_x = np.append(batch_x, [x], axis = 0)
             batch_y = np.append(batch_y, [y], axis = 0)
             
         return batch_x, batch_y
 
-class GeneratorCallback(tf.keras.callbacks.Callback):
-    def __init__(
-        self, max_tokens, start_tokens, top_k=10, print_every=5
-    ):
-        self.max_tokens = max_tokens
-        self.start_tokens = start_tokens
-        self.print_every = print_every
-        self.k = top_k
-
-    def sample_from(self, logits):
-        logits, indices = tf.math.top_k(logits, k=self.k, sorted=True)
-        indices = np.asarray(indices).astype("int32")
-        preds = tf.keras.activations.softmax(tf.expand_dims(logits, 0))[0]
-        preds = np.asarray(preds).astype("float32")
-        return np.random.choice(indices, p=preds)
-
-
-    def on_epoch_end(self, epoch, logs=None):
-        maxlen = sequence_length = 600
-        start_tokens = [_ for _ in self.start_tokens]
-        if (epoch + 1) % self.print_every != 0:
-            return
-        num_tokens_generated = 0
-        tokens_generated = []
-        while num_tokens_generated <= self.max_tokens:
-            x = start_tokens[-sequence_length:]
-            pad_len = maxlen - len(start_tokens)
-            sample_index = -1
-            if pad_len > 0:
-                x = start_tokens + [0] * pad_len
-                sample_index = len(start_tokens) - 1
-            
-            x = np.array([x])
-            y, _ = self.model.predict(x)
-            sample_token = self.sample_from(y[0][sample_index])
-            tokens_generated.append(sample_token)
-            start_tokens.append(sample_token)
-            num_tokens_generated = len(tokens_generated)
-
-        print(f"last 40 tokens of starting token:\n{self.start_tokens[-50:]}\n")
-        print(f"generated token:\n{tokens_generated}\n")
-
 class TokenAndPositionEmbedding(tf.keras.layers.Layer):
-    def __init__(self, maxlen, vocab_size, embed_dim):
+    def __init__(self, seq_len, vocab_size, embed_dim):
         super(TokenAndPositionEmbedding, self).__init__()
-        self.token_emb = tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
         self.vocab_size = vocab_size
         self.embed_dim = embed_dim
-        self.maxlen = maxlen
+        self.seq_len = seq_len
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
         
     def get_config(self):
         config = super().get_config().copy()
         config.update({
             'vocab_size': self.vocab_size,
             'embed_dim': self.embed_dim,
-            'maxlen': self.maxlen,
+            'maxlen': self.seq_len,
         })
         return config
     
-    def get_angles(self, pos, i, d_model):
-        angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
-        return pos * angle_rates
-    
     def positional_encoding(self, position, d_model):
-        angle_rates = 1 / np.power(10000, (2 * (np.arange(d_model)[np.newaxis, :]//2)) / np.float32(d_model))
-        angle_rads = np.arange(position)[:, np.newaxis] * angle_rates
+        angle_rads = 1 / np.power(10000, (2 * (np.arange(d_model)[np.newaxis, :]//2)) / np.float32(d_model))
+        angle_rads = np.arange(position)[:, np.newaxis] * angle_rads
         # sine function
         angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
         # cosine function
         angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-        pos_encoding = angle_rads[np.newaxis, ...]
-        return tf.cast(pos_encoding, dtype=tf.float32)
+        return tf.cast(angle_rads[np.newaxis, ...], dtype=tf.float32)
         
     def call(self, x):
-        maxlen = tf.shape(x)[-1]
+        seq_len = tf.shape(x)[-1]
         pos_encoding = self.positional_encoding(10000, self.embed_dim)
-        x = self.token_emb(x)
-        return x + pos_encoding[:,:maxlen,:]
+        x = self.embedding(x)
+        return x + pos_encoding[:,:seq_len,:]
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, embed_dim, num_heads=8):
-        #defining no of nodes/dim for each layer
+    def __init__(self, embed_dim, num_heads=4):
         super(MultiHeadAttention, self).__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        self.projection_dim = embed_dim // num_heads
-        self.query_dense = tf.keras.layers.Dense(embed_dim)
-        self.key_dense = tf.keras.layers.Dense(embed_dim)
-        self.value_dense = tf.keras.layers.Dense(embed_dim)
-        self.combine_heads = tf.keras.layers.Dense(embed_dim)
+        self.merge = tf.keras.layers.Dense(embed_dim)
+        self.query_layer = tf.keras.layers.Dense(embed_dim)
+        self.key_layer = tf.keras.layers.Dense(embed_dim)
+        self.value_layer = tf.keras.layers.Dense(embed_dim)
         
     def get_config(self):
         config = super().get_config().copy()
@@ -144,24 +96,19 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         return config
 
     @staticmethod
-    def causal_attention_mask(n_dest, n_src, dtype):
-        """
-        1's in the lower triangle, counting from the lower right corner.
-        """
+    def attention_mask(n_dest, n_src, dtype):
         i = tf.range(n_dest)[:, None]
         j = tf.range(n_src)
-        m = i >= j - n_src + n_dest
-        return tf.cast(m, dtype)
+        return tf.cast((i >= j - n_src + n_dest), dtype)
 
-    def attention(self, query, key, value):
-        score = tf.matmul(query, key, transpose_b=True)
+    def atten(self, query, key, value):
         dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
-        scaled_score = score / tf.math.sqrt(dim_key)
+        # scale the score
+        scaled_score = tf.matmul(query, key, transpose_b=True) / tf.math.sqrt(dim_key)
 
-        # prevent information flow from future tokens
         shape = tf.shape(scaled_score)
         dim_dest, dim_src = shape[2], shape[3]
-        attention_mask = self.causal_attention_mask(
+        attention_mask = self.attention_mask(
             dim_dest, dim_src, scaled_score.dtype
         )
         attention_mask = tf.reshape(attention_mask, [1, 1, dim_dest, dim_src])
@@ -171,56 +118,48 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         output = tf.matmul(weights, value)
         return output, weights
 
-    def separate_heads(self, x, batch_size):
-        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+    def one_head(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.embed_dim // self.num_heads))
         return tf.transpose(x, perm=[0, 2, 1, 3])
 
     def call(self, inputs):
         # x.shape = [batch_size, seq_len, embedding_dim]
         batch_size = tf.shape(inputs)[0]
-        query = self.query_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        key = self.key_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        query = self.separate_heads(query, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
-        key = self.separate_heads(key, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
-        value = self.separate_heads(value, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
-        attention, _ = self.attention(query, key, value)
-        attention = tf.transpose(attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len, num_heads, projection_dim)
-        concat_attention = tf.reshape(attention, (batch_size, -1, self.embed_dim))  # (batch_size, seq_len, embed_dim)
-        output = self.combine_heads(concat_attention)  # (batch_size, seq_len, embed_dim)
+        query = self.one_head(self.query_layer(inputs), batch_size)
+        key = self.one_head(self.key_layer(inputs), batch_size)
+        value = self.one_head(self.value_layer(inputs), batch_size)
+        attention, _ = self.atten(query, key, value)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        output = self.merge(tf.reshape(attention, (batch_size, -1, self.embed_dim)))
         return output
 
 class TransformerBlock(tf.keras.layers.Layer):
-    def __init__(self, embed_dim, num_heads, ff_dim, dropout_rate=0.1):
+    def __init__(self, embed_dim, num_heads, dense_units):
         super(TransformerBlock, self).__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        self.ff_dim = ff_dim
-        self.dropout_rate = dropout_rate
+        self.dense_units = dense_units
         
-        self.att = MultiHeadAttention(embed_dim, num_heads)
-        self.ffn = tf.keras.Sequential(
-            [tf.keras.layers.Dense(ff_dim, activation="relu"), tf.keras.layers.Dense(embed_dim),]
-        )
+        self.atten = MultiHeadAttention(embed_dim, num_heads)
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = tf.keras.layers.Dropout(dropout_rate)
-        self.dropout2 = tf.keras.layers.Dropout(dropout_rate)
+        self.dropout1 = tf.keras.layers.Dropout(0.2)
+        self.dropout2 = tf.keras.layers.Dropout(0.2)
+        self.forward_layers = tf.keras.Sequential(
+            [tf.keras.layers.Dense(units = dense_units, activation="relu"), 
+             tf.keras.layers.Dense(units = embed_dim),]
+        )
         
     def get_config(self):
         config = super().get_config().copy()
         config.update({
             'embed_dim': self.embed_dim,
             'num_heads': self.num_heads,
-            'ff_dim': self.ff_dim,
-            'dropout_rate': self.dropout_rate,
+            'dense_units': self.dense_units,
         })
         return config
 
     def call(self, inputs):
-        attention_output = self.att(inputs)
-        attention_output = self.dropout1(attention_output)
-        out1 = self.layernorm1(inputs + attention_output)
-        ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output)
-        return self.layernorm2(out1 + ffn_output)
+        atten = self.dropout1(self.atten(inputs))
+        temp = self.layernorm1(inputs + atten)
+        return self.layernorm2(self.layernorm1(inputs + atten) + self.dropout2(self.forward_layers(temp)))
