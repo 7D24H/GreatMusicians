72709563882c7bb02dd230c8d55a58611bdc6a13
Rigo <rigoyao@outlook.com>
2022-12-11 23:23:34 -0500

    update generate file

--- Transformer/generate.py
+++ Transformer/generate.py
@@ -1,38 +1,62 @@
 from model import *
-from train import *
+import tensorflow as tf
+import numpy as np
+import random
 import pretty_midi
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+mlb = MultiLabelBinarizer()
+mlb.fit([np.arange(128).tolist()])
 
-# model.load_weights("./output/classic_100641670557849_16v2f/music-gen-weight.hdf5")
 
-song_idx = random.randint(0,len(all_song_tokenised)-1)
-seq_start_at = random.randint(0,len(all_song_tokenised[song_idx])-sequence_length)   
-start_tokens = all_song_tokenised[song_idx][seq_start_at:seq_start_at + 100].tolist()
-while (start_tokens == [()]*sequence_length):
+with open('./data/jazz_pickle/all_song_tokenised.pickle', 'rb') as f:
+    processed_dataset = pickle.load(f)
+with open('./data/jazz_pickle/int_to_combi.pickle', 'rb') as f:
+    int_to_combi = pickle.load(f)
+with open('./data/jazz_pickle/vocab.pickle', 'rb') as f:
+    vocab = pickle.load(f)
+
+seq_len = 600
+vocab_size = len(vocab)
+inputs = tf.keras.layers.Input(shape=(seq_len,), dtype=tf.int32)
+embedding_layer = TokenAndPositionEmbedding(seq_len, vocab_size, 128)
+x = embedding_layer(inputs)
+x = TransformerBlock(128, 4, 128)(x)
+x = TransformerBlock(128, 4, 128)(x)
+x = TransformerBlock(128, 4, 128)(x)
+x = TransformerBlock(128, 4, 128)(x)
+x = TransformerBlock(128, 4, 128)(x)
+outputs = tf.keras.layers.Dense(vocab_size)(x)
+model = tf.keras.Model(inputs=inputs, outputs=[outputs, x])
+
+model.summary()
+model.load_weights("./output/jazz.hdf5")
+
+song_idx = random.randint(0,len(processed_dataset)-1)
+start_idx = random.randint(0,50)   
+sequence = processed_dataset[song_idx][start_idx:start_idx + 100].tolist()
+while (sequence == [()]*seq_len):
     print("Got all zeros, rerolling")
-    song_idx = random.randint(0,len(all_song_tokenised)-1)
-    seq_start_at = random.randint(0,len(all_song_tokenised[song_idx])-sequence_length)   
-    start_tokens = all_song_tokenised[song_idx][seq_start_at:seq_start_at + sequence_length].tolist()
+    song_idx = random.randint(0,len(processed_dataset)-1)
+    start_tokens = processed_dataset[song_idx][start_idx:start_idx + seq_len].tolist()
     
-ori = start_tokens.copy()
-backup = ori.copy()
-
-
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
+seq_copy = sequence.copy()
 
 def sample_from(logits):
     logits, indices = tf.math.top_k(logits, k= 10, sorted=True)
     indices = np.asarray(indices).astype("int32")
     preds = np.asarray(logits).astype("float32")
-    if(unk_tag_idx in indices):
-        unk_tag_position = np.where(indices == unk_tag_idx)[0].item()
+    # unk token
+    if(0 in indices):
+        unk_tag_position = np.where(indices == 0)[0].item()
         indices = np.delete(indices, unk_tag_position)
         preds = np.delete(preds, unk_tag_position)
-    preds = softmax(preds)
-    return np.random.choice(indices, p=preds)
+    temp = np.exp(preds - np.max(preds))
+    softmax_preds = temp / temp.sum(axis=0)
+    return np.random.choice(indices, p=softmax_preds)
 
 def convertToRoll(seq_list):
     seq_list = [int_to_combi[i] for i in seq_list]
@@ -40,34 +64,27 @@ def convertToRoll(seq_list):
     print(seq_list)
     return roll
 
+i = 0
 
-tokens_generated = []
-num_tokens_generated = 0
-num_note_to_gen = 1000
+while i <= 1000:
 
-while num_tokens_generated <= num_note_to_gen:
-
-    x = start_tokens[-sequence_length:]
-    pad_len = maxlen - len(start_tokens)
-    sample_index = -1
-    if pad_len > 0:
-        x = start_tokens + [0] * pad_len
-        sample_index = len(start_tokens) - 1
-    
+    x = sequence[-seq_len:]
+    idx = -1
+    if seq_len - len(sequence) > 0:
+        x = sequence + [0] * (seq_len - len(sequence))
+        idx = len(sequence) - 1    
     x = np.array([x])
     y, _ = model.predict(x)
-    sample_token = sample_from(y[0][sample_index])
-    tokens_generated.append(sample_token)
-    start_tokens.append(sample_token)
-    num_tokens_generated = len(tokens_generated)
-    print(f"generated {num_tokens_generated} notes")
+    sequence.append(sample_from(y[0][idx]))
+    i += 1
+    print(f"generated {i} notes")
     
-# print(f"Piano int seq generated")
-piano_roll = convertToRoll(start_tokens)
-print("-------------------------------------------")
-ori = convertToRoll(ori)
 
-def piano_roll_to_pretty_midi(piano_roll_in, fs, program=0, velocity = 64):
+piano_roll = convertToRoll(sequence)
+print("-------------------------------------------")
+seq_copy = convertToRoll(seq_copy)
+
+def piano_to_midi(piano_roll_in, fs, program=0, velocity = 64):
     piano_roll = np.where(piano_roll_in == 1, 64, 0)
     notes, _ = piano_roll.shape
     pm = pretty_midi.PrettyMIDI(initial_tempo=100.0)
@@ -76,46 +93,28 @@ def piano_roll_to_pretty_midi(piano_roll_in, fs, program=0, velocity = 64):
     # pad 1 column of zeros so we can acknowledge inital and ending events
     piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
     print(piano_roll.shape)
-    # use changes in velocities to find note on / note off events
-    velocity_changes = np.nonzero(np.diff(piano_roll).T)
-    # keep track on velocities and note on times
-    prev_velocities = np.zeros(notes, dtype=int)
+    prev = np.zeros(notes, dtype=int)
     note_on_time = np.zeros(notes)
 
-    for time, note in zip(*velocity_changes):
+    for time, note in zip(*np.nonzero(np.diff(piano_roll).T)):
         velocity = piano_roll[note, time + 1]
         time = time / fs
-        if velocity > 0:
-            if prev_velocities[note] == 0:
-                note_on_time[note] = time
-                prev_velocities[note] = velocity
-        else:
-            pm_note = pretty_midi.Note(
-                velocity=prev_velocities[note],
-                pitch=note,
-                start=note_on_time[note],
-                end=time)
-            instrument.notes.append(pm_note)
-            prev_velocities[note] = 0
+        if prev[note] == 0:
+            note_on_time[note] = time
+            prev[note] = velocity
     pm.instruments.append(instrument)
     return pm
 
-bpm = 150
-fs = 1/((60/bpm)/4)
+fs = 1/((60/150)/4)
 name = "test"
-mid_out = piano_roll_to_pretty_midi(piano_roll.T, fs=fs)
-mid_ori = piano_roll_to_pretty_midi(ori.T, fs=fs)
-midi_out_path = output_path+f"gpt-v3-id-{name}.mid"
-if midi_out_path is not None:
-        mid_out.write(midi_out_path)
-        
-midi_ori_path = output_path+f"ori-gpt-v3-id-{name}.mid"
-if midi_ori_path is not None:
-        mid_ori.write(midi_ori_path)
+output_path = "./output/"
+mid_out = piano_to_midi(piano_roll.T, fs=fs)
+mid_seq_copy = piano_to_midi(seq_copy.T, fs=fs)
+mid_out.write(output_path+f"{name}.mid")
+mid_seq_copy.write(output_path+f"ori-snapshot.mid")
 
-ori_full = all_song_tokenised[song_idx][seq_start_at:].tolist()
-ori_full = convertToRoll(ori_full)
-ori_full = piano_roll_to_pretty_midi(ori_full.T, fs=fs)
-midi_ori_full_path = output_path+f"orifull-gpt-v3-{name}.mid"
-if midi_ori_full_path is not None:
-        ori_full.write(midi_ori_full_path)
+song_full = processed_dataset[song_idx][start_idx:].tolist()
+song_full = convertToRoll(song_full)
+song_full = piano_to_midi(song_full.T, fs=fs)
+midi_song_full_path = output_path+f"ori-full-{name}.mid"
+song_full.write(midi_song_full_path)
